659a435b9a9a6dd356c4385dd46b92b690b3105f
PrintLine512 <paralet.games@yandex.ru>
2024-11-03 22:49:33 +0300

    patch

--- cli/templates/models.py
+++ cli/templates/models.py
@@ -1,5 +1,8 @@
+from sqlalchemy.dialects import postgresql
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from config import db_url
 
 engine = create_engine(db_url)
@@ -7,4 +10,8 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 server_model = Base.classes['servers']
+Session = sessionmaker(bind=engine)
+session = Session()
+server = session.query(server_model).filter_by(name="Smart").first()
+print(server.name)
 
