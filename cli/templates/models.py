18f577a678cf75b978e7f41c245e6cd63d0821a5
PrintLine512 <paralet.games@yandex.ru>
2024-11-03 21:54:43 +0300

    patch

--- cli/templates/models.py
+++ cli/templates/models.py
@@ -5,7 +5,6 @@ from config import db_url
 engine = create_engine(db_url)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-print(Base.classes.keys())
 
 server_model = Base.classes['servers']
 
